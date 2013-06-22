09e09b9048b6089974abf71bd6281f9b3893a9a7
Nicolas Macé <nicolas91mace@gmail.com>
2013-06-22 15:12:33 +0100

    made the code clearer

--- marienbad/main.cpp
+++ marienbad/main.cpp
@@ -7,6 +7,39 @@ using namespace std;
 
 typedef Vector<int> conf;
 
+// list all accessible configurations from a configuration c
+Vector<conf> list_moves(conf c)
+{
+	int n = c.size();
+	int sum = 0;
+	for(int i=0;i<n;i++) sum += c[i];
+	
+	Vector<conf> list (sum);
+	
+	conf temp;
+	int count = 0;
+	
+	for(int i=0;i<n;i++)
+	{
+		temp = c;
+				
+		for(int j=1;j<c[i];j++)
+		{
+			temp[i] -= 1;
+			
+			list[count] = temp;
+			count ++;
+		}
+		
+		temp = c.drop(i);
+		
+		list[count] = temp;
+		count ++;
+	}
+		
+	return list;
+}
+
 // return true if the configuration c at the end of the current turn makes the current player win
 bool is_winning(conf c)
 {
@@ -15,26 +48,14 @@ bool is_winning(conf c)
 	// stopping condition
 	if(c == conf (1,1)) return !win;
 	
-	// test each move row by row. If a move leads to a winning configuration, then the configuration is not winning.
+	// test each accessible configuration. If a move leads to a winning configuration, then the configuration is not winning.
 	else
 	{		
-		int n = c.size();
-		conf temp;
+		Vector<conf> list = list_moves(c);
+		int n = list.size();
 		
-		for(int i=0;i<n;i++)
-		{
-			temp = c;
-					
-			for(int j=1;j<c[i];j++)
-			{
-				temp[i] -= 1;
-				win *= !is_winning(temp);
-			}
-			
-			// delete the row that has just been tested
-			temp = c.drop(i);
-			win *= !is_winning(temp);
-		}
+		for(int i=0;i<n && win;i++) win = !is_winning(list[i]);
+		
 		return win;
 	}
 }
@@ -45,34 +66,21 @@ conf play(conf init)
 	int n = init.size();
 	
 	if(n == 1) {
-		if(init[0] == 0) cout << "Tu as gagné petit fumier. Je le savais depuis le début mais je t'ai laissé jouer pour te faire plaisir." << endl;
-		else cout << "Fin de partie. J'ai gagné. Comme prévu..." << endl;
+		cout << "Fin de partie. J'ai gagné. Comme prévu..." << endl;
 		return init.drop(0);
 	}
 	
-	else {
+	else {		
+		Vector<conf> list = list_moves(init);
+		int n = list.size();
 		
-		conf temp;
-		
-		for(int i=0;i<n;i++)
-		{
-			temp = init;
-			
-			for(int j=1;j<init[i];j++)
-			{
-				temp[i] -= 1;
-				if(is_winning(temp)) return temp;
-			}
-			
-			temp = init.drop(i);
-			if(is_winning(temp)) return temp;
-		}
+		for(int i=0;i<n;i++) if(is_winning(list[i])) return list[i];
 		
 		// if there is no winning configuration avalaible, do a random move
 		
 		srand(time(NULL));
 		
-		int col = rand() % n;
+		int col = rand() % init.size();
 		int num = rand() % init[col] + 1;
 		
 		if(num<init[col]) init[col] -= num;
@@ -144,5 +152,6 @@ int main()
 {
 	conf c (3); c[0]=5; c[1]=3; c[2]=1;
 	game(c);
+	
 	return 0; 
 }
